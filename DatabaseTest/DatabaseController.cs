789c4c40d7b62c23ce775e0355d27d064b7ebf02
Hasse Augustinussen <hasse_augu@hotmail.dk>
2018-04-18 15:01:07 +0200

    read funstion done call missing

--- DatabaseTest/DatabaseController.cs
+++ DatabaseTest/DatabaseController.cs
@@ -25,7 +25,65 @@ namespace DatabaseTest
                     conn.Open();
                     SqlCommand cmd = new SqlCommand(procedureName, conn);
                     cmd.CommandType = CommandType.StoredProcedure;
+                    foreach (KeyValuePair<string, object> arg in args)
+                    {
+                        cmd.Parameters.Add(new SqlParameter("@" + arg.Key, arg.Value));
+                    }
 
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("FEJL: " + e.Message);
+                    throw e;
+                }
+            }
+        }
+        public static object[][] RetrieveQuery(string procedureName, Dictionary<string, object> args)
+        {
+            string connectionString = string.Format("Server={0}; Database={1}; User Id={2}; Password={3};", url, database, username, password);
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            { 
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(procedureName, conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    foreach (KeyValuePair<string, object> arg in args)
+                    {
+                        cmd.Parameters.Add(new SqlParameter("@" + arg.Key, arg.Value));
+                    }
+
+                    SqlDataReader itemReader = cmd.ExecuteReader();
+                    List<object> cols = new List<object>();
+                    List<object[]> rows = new List<object[]>();
+                    if (itemReader.HasRows)
+                    {
+                        for (int i = 0; i < itemReader.FieldCount; i++)
+                        {
+                            cols.Add(itemReader.GetName(i));
+                        }
+
+                        while (itemReader.Read())
+                        {
+                            object[] row = new object[itemReader.FieldCount];
+                            itemReader.GetValues(row);
+                            rows.Add(row);
+                        }
+                    }
+
+                    object[][] result = new object[1 + rows.Count][];
+                    result[0] = cols.ToArray();
+                    for (int i = 0; i < rows.Count; i++)
+                    {
+                        result[1 + i] = rows[i].ToArray();
+                    }
+                    return result;
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("FEJL: " + e.Message);
+                    throw e;
                 }
             }
         }
